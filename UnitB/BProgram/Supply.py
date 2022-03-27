96cf208cd9988fc28603caefd5d081c74a4fe9ec
ryangrayson1 <rtg5xkh@virginia.edu>
2022-03-26 22:25:51 -0400

    bprogram?

---
+++ UnitB/BProgram/Supply.py
@@ -0,0 +1,38 @@
+# CS4102 Spring 2022 - Unit B Programming
+#################################
+# Collaboration Policy: You are encouraged to collaborate with up to 3 other
+# students, but all work submitted must be your own independently written
+# solution. List the computing ids of all of your collaborators in the
+# comments at the top of each submitted file. Do not share written notes,
+# documents (including Google docs, Overleaf docs, discussion notes, PDFs), or
+# code. Do not seek published or online solutions, including pseudocode, for
+# this assignment. If you use any published or online resources (which may not
+# include solutions) when completing this assignment, be sure to cite them. Do
+# not submit a solution that you are unable to explain orally to a member of
+# the course staff. Any solutions that share similar text/code will be
+# considered in breach of this policy. Please refer to the syllabus for a
+# complete description of the collaboration policy.
+#################################
+# Your Computing ID:
+# Collaborators:
+# Sources: Introduction to Algorithms, Cormen
+#################################
+
+class Supply:
+    def __init__(self):
+        return
+
+    # This is the method that should set off the computation
+    # of the supply chain problem.  It takes as input a list containing lines of input
+    # as strings.  You should parse that input and then call a
+    # subroutine that you write to compute the total edge-weight sum
+    # and return that value from this method
+    #
+    # @return the total edge-weight sum of a tree that connects nodes as described
+    # in the problem statement
+    def compute(self, file_data):
+        edgeWeightSum = 0
+
+        # your function to compute the result should be called here
+
+        return edgeWeightSum
